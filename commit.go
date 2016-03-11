11fdfa40f61c099354f388fca3da5ef911524b1f
frankw <fwessels@xs4all.nl>
2016-03-11 12:57:42 +0100

    Refactor commit code to allow test code private access to create forks

--- commit.go
+++ commit.go
@@ -2,6 +2,8 @@ package s3git
 
 import (
 	"encoding/hex"
+	"errors"
+	"time"
 	"github.com/s3git/s3git-go/internal/core"
 	"github.com/s3git/s3git-go/internal/kv"
 )
@@ -9,29 +11,70 @@ import (
 type Commit struct {
 	Hash    string
 	Message string
+	TimeStamp string
+	Parent	string
 }
 
 // Perform a commit for the repository
 func (repo Repository) Commit(message string) (hash string, empty bool, err error) {
+	return repo.commit(message, "master", []string{})
+}
 
-	list, err := kv.ListStage()
-	if err != nil {
-		return "", false, err
-	}
+// Perform a commit for the named branch of the repository
+func (repo Repository) CommitToBranch(message, branch string) (hash string, empty bool, err error) {
+	return repo.commit(message, branch, []string{})
+}
+
+func (repo Repository) commit(message, branch string, parents []string) (hash string, empty bool, err error) {
+
+	warmParents := []string{}
+	coldParents := []string{}
 
 	commits, err := kv.ListTopMostCommits()
 	if err != nil {
 		return "", false, err
 	}
 
-	// TODO: Handle case with multiple parents
-	parents := []string{}
-	for c := range commits {
-		parents = append(parents, hex.EncodeToString(c))
+	if len(parents) == 0 {
+		for c := range commits {
+			warmParents = append(warmParents, hex.EncodeToString(c))
+		}
+		if len(warmParents) > 1 {
+			// TODO: Do extra check whether the trees are the same, in that case we can safely ignore the warning
+			return "", false, errors.New("Multiple top most commits founds as parents")
+		}
+	} else {
+		for c := range commits {
+			p := hex.EncodeToString(c)
+			if contains(parents, p) {
+				warmParents = append(warmParents, p)
+			} else {
+				coldParents = append(coldParents, p)
+			}
+		}
+	}
+
+	return repo.commitWithWarmAndColdParents(message, branch, warmParents, coldParents)
+}
+
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
+
+func (repo Repository) commitWithWarmAndColdParents(message, branch string, warmParents, coldParents []string) (hash string, empty bool, err error) {
+
+	list, err := kv.ListStage()
+	if err != nil {
+		return "", false, err
 	}
 
 	// Create commit object on disk
-	commitHash, empty, err := core.StoreCommitObject(message, parents, list, []string{})
+	commitHash, empty, err := core.StoreCommitObject(message, branch, warmParents, coldParents, list, []string{})
 	if err != nil {
 		return "", false, err
 	}
