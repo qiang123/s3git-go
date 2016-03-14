a11f57d63e49c73becfe54d039fe8805f40458dd
frankw <fwessels@xs4all.nl>
2016-03-14 11:37:22 +0100

    Added comments to pull test case (but needs more work)

--- pull_test.go
+++ pull_test.go
@@ -3,16 +3,21 @@ package s3git
 import (
 	"fmt"
 	"github.com/s3git/s3git-go/internal/core"
-	_ "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
 	"strings"
 	"testing"
 	"time"
 )
 
+// TODO: Think about how to deal with S3 test bucket (or make fake 'back-end' that runs locally in temp storage)
+// TODO: Do proper test that does not rely on external (S3) state
 func TestPull(t *testing.T) {
 	repo, path := setupRepo()
 	fmt.Println(path)
 	//defer teardownRepo(path)
+
+	err := repo.Pull()
+	assert.Nil(t, err)
 
 	for i := 0; i < 10; i++ {
 		repo.Add(strings.NewReader(fmt.Sprintf("hello s3git: %d, %s", i, time.Now())))
@@ -24,5 +29,6 @@ func TestPull(t *testing.T) {
 
 	repo.Push()
 
-	repo.Pull()
+	err = repo.Pull()
+	assert.Nil(t, err)
 }
