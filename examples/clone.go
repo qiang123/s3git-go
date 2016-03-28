5f7f5c5f241dc60c26553f6284e99086469ff4c4
frankw <fwessels@xs4all.nl>
2016-03-28 16:28:29 +0200

    Example for cloning

---
+++ examples/clone.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"github.com/s3git/s3git-go"
+	"io/ioutil"
+)
+
+func main() {
+
+	dir, _ := ioutil.TempDir("", "s3git")
+
+	options := []s3git.CloneOptions{}
+	options = append(options, s3git.CloneOptionSetAccessKey("AKIAJYNT4FCBFWDQPERQ"))
+	options = append(options, s3git.CloneOptionSetSecretKey("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"))
+
+	repo, _ := s3git.Clone("s3://s3git-spoon-knife", dir, options...)
+
+	list, _ := repo.List("")
+
+	for l := range list {
+		fmt.Println(l)
+	}
+}
