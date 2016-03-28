16ac12112e78d4f8a8d92296516452646d1aa593
frankw <fwessels@xs4all.nl>
2016-03-28 16:32:18 +0200

    Add comments for examples

--- examples/clone.go
+++ examples/clone.go
@@ -14,8 +14,10 @@ func main() {
 	options = append(options, s3git.CloneOptionSetAccessKey("AKIAJYNT4FCBFWDQPERQ"))
 	options = append(options, s3git.CloneOptionSetSecretKey("OVcWH7ZREUGhZJJAqMq4GVaKDKGW6XyKl80qYvkW"))
 
+	// Clone a repo
 	repo, _ := s3git.Clone("s3://s3git-spoon-knife", dir, options...)
 
+	// List contents
 	list, _ := repo.List("")
 
 	for l := range list {
--- examples/create.go
+++ examples/create.go
@@ -11,12 +11,16 @@ func main() {
 
 	dir, _ := ioutil.TempDir("", "s3git")
 
+	// Create repo
 	repo, _ := s3git.InitRepository(dir)
 
+	// Add some data
 	repo.Add(strings.NewReader("hello s3git"))
 
+	// Commit changes
 	repo.Commit("Initial commit")
 
+	// List commits
 	commits, _ := repo.ListCommits("")
 
 	for commit := range commits {
