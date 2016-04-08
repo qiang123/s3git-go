026bb916b40d0872d69d5d94ff49bbea872327de
frankw <fwessels@xs4all.nl>
2016-04-08 14:20:19 +0200

    Initial test case for cas

--- internal/cas/writer_test.go
+++ internal/cas/writer_test.go
@@ -15,3 +15,83 @@
  */
 
 package cas
+
+import (
+	"os"
+	"io"
+	"io/ioutil"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"strings"
+	"bytes"
+	"github.com/s3git/s3git-go/internal/config"
+	"github.com/s3git/s3git-go/internal/kv"
+)
+
+func TestWriteSingleChunk(t *testing.T) {
+
+	path := setupRepo(t)
+	defer teardownRepo(path)
+
+	input := "hello s3git\n"
+
+	rootKeyStr := writeTo(t, strings.NewReader(input))
+	output := readBack(t, rootKeyStr)
+
+	assert.Equal(t, input, output, "Input and output are different")
+}
+
+func writeTo(t *testing.T, r io.Reader) string {
+
+	cw := MakeWriter(BLOB)
+	defer cw.Close()
+
+	_, err := io.Copy(cw, r)
+	assert.Nil(t, err)
+
+	rootKeyStr, _, _, err := cw.Flush()
+	assert.Nil(t, err)
+
+	return rootKeyStr
+}
+
+func readBack(t *testing.T, hash string) string {
+
+	cr := MakeReader(hash)
+	assert.NotEmpty(t, cr)
+
+	buf := bytes.NewBuffer(nil)
+
+	size := 0
+	array := make([]byte, config.Config.ChunkSize)
+	for {
+		read, err := cr.Read(array)
+		size += read
+		if read > 0 {
+			_, err := buf.Write(array[:read])
+			assert.Nil(t, err)
+		}
+		if err == io.EOF {
+			break
+		}
+	}
+
+	return string(buf.Bytes())
+}
+
+func setupRepo(t *testing.T) (string) {
+	path, _ := ioutil.TempDir("", "s3git-cas-")
+
+	config.SaveConfig(path)
+
+	success, err := config.LoadConfig(path)
+	assert.Nil(t, err)
+	assert.Equal(t, true, success, "Failed to init repo")
+	kv.OpenDatabase()
+
+	return path
+}
+
+func teardownRepo(path string) {
+	defer os.RemoveAll(path)
+}
